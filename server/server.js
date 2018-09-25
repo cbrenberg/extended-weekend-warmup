14775c6ab20d53e75880dea4ed4584893326c05f
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-25 16:53:56 -0500

    create pool connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,23 @@
 console.log('Wooooo');
 const express = require('express');
 const app = express();
+const pg = require('pg');
+
+const pool = new pg.Pool({
+  host: 'localhost', // Server hosting the postgres database
+  port: 5432, 
+  database: 'spooky_things',
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000
+})
+
+pool.on('connect', () => {
+  console.log('Postgres up and running!');
+})
+
+pool.on('error', (error) => {
+  console.log('Postgres error:', error);
+})
 
 const PORT = 5000;
 
