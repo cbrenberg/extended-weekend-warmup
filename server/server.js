1290c8fb3ecad2b9f6b7da1fac76dd0859e039e2
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-25 16:40:46 -0500

    logs Boo at /greeting

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ console.log('Wooooo');
 const express = require('express');
 const app = express();
 
-
-
 const PORT = 5000;
 
-console.log(`Up and running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Up and running on port ${PORT}`);
+})
 
+app.get('/greeting', (req, res) => {
+  res.send('Boo');
+})
