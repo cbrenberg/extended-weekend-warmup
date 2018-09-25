25bafb5c874e26d72fe48241908382e00698bba0
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-25 16:47:57 -0500

    add costumes to database

--- database.sql
+++ database.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE "spooky_things";
 
+CREATE TABLE "costumes" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(50) NOT NULL
+);
+
+INSERT INTO "costumes" ("name")
+	VALUES ('Skeleton'), ('Pumpkin'), ('Space Pirate');
