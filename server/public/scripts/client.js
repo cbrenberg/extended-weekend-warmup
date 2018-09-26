ceb2add02d8a5462dd452235caee2c9ad55b6294
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-25 19:56:26 -0500

    bring in angularJS, create app and controller

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,8 @@
 console.log('Trick or Treat');
+
+const app = angular.module("HalloweenApp", []);
+
+app.controller('HalloweenController', function () {
+  let vm = this;
+  vm.message = "angular loaded!"
+})
