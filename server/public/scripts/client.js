092aca62f587d970aa0b0b702322f9055735ec32
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-25 20:26:01 -0500

    display costume list on DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,7 +2,23 @@ console.log('Trick or Treat');
 
 const app = angular.module("HalloweenApp", []);
 
-app.controller('HalloweenController', function () {
+app.controller('HalloweenController', ['$http', function ($http) {
+  console.log('HalloweenController loaded');
+
   let vm = this;
-  vm.message = "angular loaded!"
-})
+  vm.message = "angular loaded!";
+  vm.costumes = [];
+
+  vm.getCostumes = function () {
+    $http.get('/costumes')
+    .then(function (response) {
+      console.log('Back from /costumes with:', response);
+      vm.costumes = response.data;
+    })
+    .catch(function (error) {
+      console.log("error retrieving costumes", error);
+    });
+  }
+
+  vm.getCostumes();
+}]);
